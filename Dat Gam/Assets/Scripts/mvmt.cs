41ef3b5beac72a2a7c80a46bbffc4f8f022d770e
Adam Gallina <adamg461@gmail.com>
2018-01-15 17:13:29 -0800

    Player sprite integrated, movement added *movement drifts, needs to be made into linear movement*

---
+++ Dat Gam/Assets/Scripts/mvmt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mvmt : MonoBehaviour {
+
+    float speed = 1.0f;
+    float x = 0;
+    float y = 0;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Move_Hor", x);
+        animator.SetFloat("Move_Ver", y);
+
+        var move = new Vector3(x, y, 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
