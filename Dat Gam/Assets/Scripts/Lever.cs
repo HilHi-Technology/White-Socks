b8347c5b083da608ffc24d8264a63c4a2e5faa38
Adam Gallina <adamg461@gmail.com>
2018-01-29 16:37:24 -0800

    Added lever, toggled walls, floor

---
+++ Dat Gam/Assets/Scripts/Lever.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour {
+
+    public int pressed;
+    public static Lever instance;
+    public int active;
+
+    bool touching;
+    bool e;
+
+    Animator anim;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        instance = this;
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        touching = true;
+    }
+	void OnTriggerExit2D(Collider2D other)
+    {
+        touching = false;
+    }
+
+
+	void Update ()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            e = true;
+        }
+        else
+        {
+            e = false;
+        }
+
+        if (touching && e)
+        {
+            pressed = -pressed;
+            anim.SetInteger("Pressed", pressed);
+        }
+    }
+}
