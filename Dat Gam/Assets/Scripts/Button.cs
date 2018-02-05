e61fd28928b07cf3b716a5778e9c033dbbd6887d
Adam Gallina <adamg461@gmail.com>
2018-02-05 11:40:24 -0800

    Incorporated updated sleeping sprites, started work on a button

---
+++ Dat Gam/Assets/Scripts/Button.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public int pressed;
+    public static Button instance;
+    public int active;
+
+    bool touching;
+    bool e;
+
+    public float delay;
+
+    Animator anim;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        instance = this;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        touching = true;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        touching = false;
+    }
+
+    void timeWait()
+    {
+        pressed = -pressed;
+        anim.SetInteger("Pressed", pressed);
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown("e")) { e = true; } else { e = false; }
+
+        if (touching && e)
+        {
+            pressed = -pressed;
+            anim.SetInteger("Pressed", pressed);
+
+            Invoke("timeWait", delay);
+        }
+    }
+}
--- Dat Gam/Assets/Scripts/CameraController.cs
+++ Dat Gam/Assets/Scripts/CameraController.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
-
     private Vector3 offset;
+
+    bool changed = false;
+    public List<Color> colors;
+
+    Camera cam;
 
 	void Start ()
     {
         offset = transform.position - player.transform.position;
-	}
+        cam = GetComponent<Camera>();
+    }
 	
 	void LateUpdate ()
     {
         transform.position = player.transform.position + offset;
+
+        if (Mvmt.instance.dreaming)
+        {
+            if (!changed)
+            {
+                cam.backgroundColor = colors[Random.Range(0,colors.Count)];
+                changed = true;
+            }
+        }
+        else
+        {
+            cam.backgroundColor = Color.gray;
+            changed = false;
+        }
 	}
 }
