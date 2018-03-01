b8714372c444d7ceeb484502087daac861789d4f
Adam Gallina <adamg461@gmail.com>
2018-02-28 17:33:14 -0800

    *mostly* fixed the nurse

--- Dat Gam/Assets/Scripts/NurseController.cs
+++ Dat Gam/Assets/Scripts/NurseController.cs
@@ -5,48 +5,56 @@ using UnityEngine;
 public class NurseController : MonoBehaviour {
 
     public float speed;
+    public float errorMargin;
 
     public List<Vector3> pathway;
     int location = 0;
     int total = 0;
     bool moving = false;
-
-    bool hit = false;
+    Transform previous;
 
     Vector3 target;
     Vector3 place;
 
+    Animator anim;
+
     void Start ()
     {
         total = pathway.Count;
+
+        anim = GetComponent<Animator>();
     }
 	
 	void Update ()
     {
+        float step = speed * Time.deltaTime;
+
+        previous = transform;
+
         if (!moving)
         {
             location += 1;
-            if (location > total - 1)
-            {
-                location = 0;
-            }
-
+            if (location > total - 1) { location = 0; }
             target = pathway[location];
+
             moving = true;
-            hit = false;
         }
         else
         {
-            while (!hit)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, target, speed);
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
 
-                if (transform.position == target)
-                {
-                    hit = true;
-                    moving = false;
-                }
+            if (Mathf.Abs(transform.position.x - target.x) < errorMargin && Mathf.Abs(transform.position.y - target.y) < errorMargin)
+            {
+                moving = false;
             }
         }
-	}
+        
+        float y = Mathf.Sign(transform.position.y - previous.position.y);
+        float x = Mathf.Sign(transform.position.x - previous.position.x);
+
+        if (y != 0) { anim.SetBool("ver?", true); } else { anim.SetBool("ver?", false); }
+        anim.SetInteger("x", Mathf.RoundToInt(x));
+        anim.SetInteger("y", Mathf.RoundToInt(y));
+
+    }
 }
